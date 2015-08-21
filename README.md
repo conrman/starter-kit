4ce68196f61f00de9e1ae20f0c1734c29adc607c
Connor Freeman <connormfreeman@gmail.com>
2015-08-20 23:42:37 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Starter Kit
+
+This is my personal set of tools for HTML, CSS, and JS.
---
+++ gulpfile.babel.js
@@ -0,0 +1,227 @@
+import fs from 'fs';
+import path from 'path';
+import del from 'del';
+import runSequence from 'run-sequence';
+import autoprefixer from 'autoprefixer';
+import browserSync from 'browser-sync';
+import requireDir from 'require-dir';
+import swPrecache from 'sw-precache';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import pkg from './package.json';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+const tasks = requireDir('./tasks');
+
+/*==========  IMAGES  ==========*/
+
+// Optimize images
+gulp.task('images', () => {
+  return gulp.src('app/assets/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/assets/images'))
+    .pipe($.size({
+      title: 'images'
+    }));
+});
+
+/*==========  FONTS  ==========*/
+
+// Copy web fonts to dist
+gulp.task('fonts', () => {
+  return gulp.src(['app/assets/fonts/**'])
+    .pipe(gulp.dest('dist/assets/fonts'))
+    .pipe($.size({title: 'fonts'}));
+});
+
+/*==========  JSHINT  ==========*/
+
+gulp.task('jshint', () => {
+  return gulp.src('app/assets/scripts/**/*.js')
+    .pipe(reload({
+      stream: true,
+      once: true
+    }))
+    .pipe($.jshint({ esnext: true }))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+
+/*==========  SCRIPTS  ==========*/
+
+// Concatenate and minify JavaScript
+gulp.task('scripts', () => {
+  return gulp.src(['./app/assets/scripts/**/*.js'])
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.concat('main.min.js'))
+    .pipe($.uglify({preserveComments: 'some'}))
+    // Output files
+    .pipe($.sourcemaps.write('../../.tmp'))
+    .pipe(gulp.dest('dist/assets/scripts'))
+    .pipe($.size({title: 'scripts'}));
+});
+
+
+/*==========  STYLES  ==========*/
+
+gulp.task('styles', () => {
+  let processors = [
+    autoprefixer({
+      browsers: 'last 1 version'
+    })
+  ];
+
+  return gulp.src('app/assets/styles/**/*.css')
+    .pipe($.sourcemaps.init())
+    .pipe($.postcss(processors))
+    .pipe($.cssnano())
+    .pipe($.sourcemaps.write('../../.tmp'))
+    .pipe(gulp.dest('dist/assets/styles'))
+    .pipe(reload({
+      stream: true
+    }))
+    .pipe($.size({title: 'styles'}));
+});
+
+/*==========  HTML  ==========*/
+
+gulp.task('html', () => {
+  const assets = $.useref.assets({
+    searchPath: '{.tmp,app}'
+  });
+
+  return gulp.src('app/**/*.html')
+    .pipe(assets)
+    // Remove any unused CSS
+    // Note: If not using the Style Guide, you can delete it from
+    // the next line to only include styles your project uses.
+    .pipe($.if('*.css', $.uncss({
+      html: [
+        'app/index.html'
+      ],
+      // CSS Selectors for UnCSS to ignore
+      ignore: []
+    })))
+
+  // Concatenate and minify styles
+  // In case you are still using useref build blocks
+  .pipe($.if('*.css', $.cssnano()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+
+  // Minify any HTML
+  .pipe($.if('*.html', $.minifyHtml()))
+    // Output files
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({
+      title: 'html'
+    }));
+});
+
+/*==========  CLEAN  ==========*/
+
+// Clean output directory
+gulp.task('clean', cb => del(['.tmp', 'dist/*', '!dist/.git'], {
+  dot: true
+}, cb));
+
+/*==========  COPY  ==========*/
+
+// Copy all files at the root level (app)
+gulp.task('copy', () => {
+  return gulp.src([
+    'app/*',
+    '!app/*.html',
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'copy'}));
+});
+
+/*==========  SERVE  ==========*/
+
+// Watch files for changes & reload
+gulp.task('serve', ['styles'], () => {
+  browserSync({
+    notify: false,
+    // Customize the BrowserSync console logging prefix
+    logPrefix: 'APP',
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['.tmp', 'app']
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/assets/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['app/assets/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/assets/images/**/*'], reload);
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], () => {
+  browserSync({
+    notify: false,
+    logPrefix: 'WSK',
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: 'dist',
+    baseDir: 'dist'
+  });
+});
+
+/*==========  SERVICE WORKER  ==========*/
+
+gulp.task('generate-service-worker', cb => {
+  const rootDir = 'dist';
+
+  swPrecache({
+    // Used to avoid cache conflicts when serving on localhost.
+    cacheId: pkg.name || 'starter-kit',
+    staticFileGlobs: [
+      // Add/remove glob patterns to match your directory setup.
+      `${rootDir}/assets/fonts/**/*.woff`,
+      `${rootDir}/assets/images/**/*`,
+      `${rootDir}/assets/scripts/**/*.js`,
+      `${rootDir}/assets/styles/**/*.css`,
+      `${rootDir}/*.{html,json}`
+    ],
+    // Translates a static file path to the relative URL that it's served from.
+    stripPrefix: path.join(rootDir, path.sep)
+  }, (err, swFileContents) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    const filepath = path.join(rootDir, 'service-worker.js');
+
+    fs.writeFile(filepath, swFileContents, err => {
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      cb();
+    });
+  });
+});
+
+/*==========  DEFAULT TASK  ==========*/
+
+// Build production files, the default task
+gulp.task('default', ['clean'], cb => {
+  runSequence(
+    'styles', ['jshint', 'html', 'scripts', 'images', 'fonts', 'copy'],
+    cb
+  );
+});
