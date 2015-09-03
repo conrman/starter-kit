bf6492a43b5c34fdf00d588f34bbead4dba60a93
Connor Freeman <connormfreeman@gmail.com>
2015-09-03 14:32:01 -0500

    added bootstrap 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -193,7 +193,7 @@ gulp.task('clean', del(path.dist));
 /*==========  WATCH  ==========*/
 
 // Watch files for changes & reload
-gulp.task('watch', function() {
+gulp.task('watch', ['build'], function() {
   browserSync.init({
     server: {
       baseDir: path.dist
@@ -203,7 +203,8 @@ gulp.task('watch', function() {
   gulp.watch([path.source + 'scripts/**/*'], ['jshint', 'scripts']);
   gulp.watch([path.source + 'fonts/**/*'], ['fonts']);
   gulp.watch([path.source + 'images/**/*'], ['images']);
-  gulp.watch([path.source + 'templates/**/*'], ['templates']);
+  gulp.watch([path.source + 'templates/**/*'], ['templates']).on('change', browserSync.reload);
+  gulp.watch(['*.jade'], ['templates']).on('change', browserSync.reload);
   gulp.watch(['bower.json', 'assets/manifest.json'], ['build']);
 });
 
